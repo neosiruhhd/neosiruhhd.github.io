ff4f2e9b39ea085292c46091bba169b909c41a25
neosiruhhd <s4171454@student.rmit.edu.au>
2026-05-12 22:43:51 +1000

    Update script.js
    churning through the JS overhaul, fucking crazy learnings to be made

--- Assignment2/script.js
+++ Assignment2/script.js
@@ -1,34 +1,131 @@
-// ELEMENT REFERENCES -- instantiating a bunch of reference points for js to draw from the DOM (various html elements within the system hierarchy)
+// ELEMENT REFERENCES -- instantiating a bunch of reference points for js to draw from the DOM (various html elements within the system hierarchy), trying to maintain the same labellings but with camel casing
 
 const currentlyPlaying =
   document.querySelector("currently-playing");
 
 const progressBar =
-  document.querySelector("#progress-bar")
+  document.querySelector("#progress-bar");
 
 const progressBarContainer =
-  document.querySelector("progress-bar-container")
+  document.querySelector("progress-bar-container");
 
+// as in, play OR pause button
 const playPauseButton =
-  document.querySelector("#play-pause-button")
+  document.querySelector("#play-pause-button");
 
+// as in, play OR pause img
 const playPauseImg =
-  document.querySelector("#play-pause-img")
+  document.querySelector("#play-pause-img");
 
 const currentTime =
-  document.querySelector("#current-time")
+  document.querySelector("#current-time");
 
 const duration =
-  document.querySelector("#duration")
+  document.querySelector("#duration");
 
 const quoteText =
-  document.querySelctor("#quote-text")
+  document.querySelctor("#quote-text");
 
 const previousButton =
-  document.querySelector("#previous-button")
+  document.querySelector("#previous-button");
 
 const nextButton =
-  document.querySelector("#next-button")
+  document.querySelector("#next-button");
+
+// PLAY / PAUSE -- functions to enable the user to play and pause the audio, this includes changing the image of the button which i need to change at some point as i do not have them rn
+
+// this event listener is to recognise when the user physically clicks the button
+playPauseButton.addEventListener
+  ("click",
+  togglePlayback);
+
+  // then it will activate the if-else function 'togglePlayback'
+function togglePlayback() {
+
+  // in the scenario where the audio has been paused, it will:
+  if (currentlyPlaying.paused) {
+    
+    // set the button into 'play' state, as in, ready to be played
+    currentlyPlaying.play();
+    // display the play icon
+    playPauseImg.src = TO BE ADDED;
+    playPauseImg.alt = "Play Icon";
+  
+    // if the audio is still playing
+  } else {
+   
+    // keep the button in the 'pause' state, as in, ready to be paused
+    currentlyPlaying.pause();
+    // display the pause icon
+    playPauseImg.src = TO BE ADDED;
+    playPauseImg.alt = "Pause Icon";
+  }
+
+};
+
+// RESET ICON -- for when the song ends naturally, turn into 'play' state
+// leaving functioned unnamed since its a oneoff
+currentlyPlaying.addEventListener(
+  "ended",
+  function () {
+    playPauseImg.src TO BE ADDED;
+    playPauseImg.alt "Play Icon";
+  }
+);
+
+// TIME FORMATTING -- code to convert the time from purely seconds to m:ss format
+// this function actually needs the parameter for time since it only operates based off that information
+function formatTime(time) {
+// isNaN checks if time is even in existence as a variable, if it isnt, show the default visual of 0:00
+  if (isNaN(time)) return "0:00";
+  // making the const within the function to separate it from the rest of the code, convinient!
+  // Math.floor is a two parter: 'Math' is an object that contains various mathematical functions and constants, 'floor' is one of those Math functions.
+  // written together, it rounds numbers down to the nearest whole integer, so we use this in both determining what is the current minute and second of the song we are playing
+  const minutes =
+    Math.floor(time / 60)
+
+  const seconds =
+    // the % is a bit weird, it gives you the REMAINING value after dividing. so like 10 % 3, would give you a remainder of 1, since 3 can go into 10 three times, and after that youre left with 1.
+    // what its doing here is taking the time value (which is the whole song in length of seconds) and then after dividing it by its minutes, taking the value of the remaining seconds, CRAZY
+    Math.floor(time % 60)
+      .toString()
+      // if the number is less than 10, adds a leading 0 to keep the seconds timer always showing 2 integers for visual consistency
+      .padStart(2, "0");
+// backticks, not quotation marks because this is not a regular string, its a template string, meaning its a string created from variables of the code and thus interpolates its outputted string
+  return `${minutes}:${seconds}`;
+}
+
+
+// PROGRESS BAR -- code to update the progress bar as the song plays
+// 'timeupdate' is a js media event that responds every time the playback position of the song changes, but it only recognises this change logically so, everytime it does i need it to run the 'updateProgress' function which updates the progress bar visually
+currentlyPlaying.addEventListener(
+  "timeupdate",
+  updateProgress
+);
+
+function updateProgress() {
+
+  // keeping consts within their functions
+  const progress =
+  // the current time of the song, divided by the total duration, then multiplied by 100.
+    (currentlyPlaying.currentTime / duration) * 100;
+
+  // the .style.width overrides any styling directions from the CSS that may have been previously stated, and instead changes the html element directly, super neat.
+  // the amount of width is determined by simply adding a percentage sign to the end of the progress float
+  progressBar.style.width = progress + "%";
+
+  // 
+  currentTime.textContent =
+    formatTime(currentlyPlaying.currentTime);
+
+  duration.textContent =
+    formatTime(currentlyPlaying.duration);
+
+}
+
+
+
+
 
 // const videoList = [
 //   { id: 1, src: "stardust.mp4" },
